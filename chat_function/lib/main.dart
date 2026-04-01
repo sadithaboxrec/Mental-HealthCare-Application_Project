572bf2254853bf3442e5f946c7cb03fdd8e9f633
senali2001 <rbnsenali@students.nsbm.ac.lk>
2026-04-02 01:15:06 +0530

    remove debugin banner

--- chat_function/lib/main.dart
+++ chat_function/lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, // ✅ HERE
 theme: ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
