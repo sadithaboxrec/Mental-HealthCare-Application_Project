13ce510baae8acf385e95260a262c2e85cdcbe20
senali2001 <rbnsenali@students.nsbm.ac.lk>
2026-04-10 23:31:39 +0530

    update doc chat page

--- chat_function/lib/page/doctor_chatpage.dart
+++ chat_function/lib/page/doctor_chatpage.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:chat_function/screens/homescreen.dart';//need to imort s this
 import 'package:chat_function/CustomUI/CustomCard.dart';
+import 'package:chat_function/model/doctorchatmodel.dart';  //need to imort s this
 
 class DoctorChatPage extends StatefulWidget {
   const DoctorChatPage({Key? key}) : super(key: key);
@@ -12,6 +13,43 @@ class DoctorChatPage extends StatefulWidget {
 
 
 class _DoctorChatPageState extends State<DoctorChatPage> {
+List<Doctorchatmodel> chats = [
+  Doctorchatmodel(
+
+    name:'Mr.Prabath Perera',
+    icon: 'assets/siyana.avif',
+    message: 'Hello',
+    time: '10:30 AM',
+    currentMessage: 'hi all',
+
+
+
+  ),
+  Doctorchatmodel(
+
+    name:'Mr.Ramanayaka',
+    icon: 'assets/siyana.avif',
+    message: 'Hello',
+    time: '10:30 AM',
+    currentMessage: 'hi all',
+
+
+
+  ),
+  Doctorchatmodel(
+
+    name:'Mr.Perera',
+    icon: 'assets/siyana.avif',
+    message: 'Hello',
+    time: '10:30 AM',
+    currentMessage: 'hi all',
+
+
+
+  ),
+
+];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,12 +57,9 @@ class _DoctorChatPageState extends State<DoctorChatPage> {
         onPressed: () {},
         child: Icon(Icons.chat),
       ),
-      body:ListView(
-        children: [
-          CustomCard(),
-          CustomCard(),
-          CustomCard(),
-        ],
+      body:ListView.builder(
+        itemCount: chats.length,
+        itemBuilder: (context, index)=> CustomCard(doctorchatmodel: chats[index]),
       ),
     );
   }
