c61a1dedc23076da2e91cbe4e9442b41f9d8522d
senali2001 <rbnsenali@students.nsbm.ac.lk>
2026-04-19 15:46:05 +0530

    navigation

--- mental_health_support_app/lib/core/navigation/navigation_helper.dart
+++ mental_health_support_app/lib/core/navigation/navigation_helper.dart
@@ -5,7 +5,7 @@ import '../../test_presentation/screens/test_login_screen.dart';
 import '../../test_presentation/screens/test_doctor_screen.dart';
 // import '../../test_presentation/screens/test_patient_screen.dart';
 //  import '../../test_presentation/screens/test_counselor_screen.dart';
-import '../../test_presentation/screens/counselor/test_counselor_screen.dart';
+//import '../../test_presentation/screens/counselor/test_counselor_screen.dart';
 // import '../../test_presentation/screens/test_guardian_screen.dart';
 // import '../../presentation/screens/login_screen.dart';
 // import '../../test_presentation/screens/doctor/test_doctor_home.dart';
@@ -15,7 +15,9 @@ import '../../test_presentation/screens/doctor/test_doctor_root.dart';
 import '../../test_presentation/screens/patient/test_patient_root.dart';
 
 // presentation
+import '../../presentation/screens/counselor/counselor_home.dart';
 import '../../presentation/screens/guardian/guardian_home.dart';
+
 
 
 
@@ -71,7 +73,8 @@ class NavigationHelper {
         // return TestGuardianHome(user: user);
         return GuardianHome(user: user);
       case 'counselor':
-        return TestCounselorScreen(user: user);
+      //  return TestCounselorScreen(user: user);
+      return CounselorHome(user: user);
       default:
         return const TestLoginScreen();
     }
